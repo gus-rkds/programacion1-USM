69359e96d97b977076643eaffac563e802f94efc
gusrkds <gusalv2001@gmail.com>
2023-04-12 18:17:16 -0400

    uptade parcial gus

--- parcialgus.cpp
+++ parcialgus.cpp
@@ -1,4 +1,9 @@
 /*
+Este programa imprime los primeros 20 numeros a partir del 100000 que cumplen las siguientes condiciones:
+- La raiz cuadrada del numero es un numero primo
+- La raiz cuadrada del numero termina en 7
+- La raiz cuadrada del numero es entera
+
 Programa elaborado por Gustavo Alviarez
 Universidad Santa María
 Programación I
@@ -24,15 +29,15 @@ bool esPrimo(int num)
 }
 
 main() {
-    // contador para saber cuantos numeros se han encontrado
+    // contador que va incrementando para saber cuantos numeros se han encontrado
     int contador = 0;
 
-    // ciclo que recorre los numeros desde 500000 hasta que se encuentren los 10 numeros que cumplan las condiciones
-    for (int i = 10000; contador < 20; i++) {
-        // raiz cuadrada del numero en su version double y int para verificar si es entero
+    // ciclo que recorre los numeros desde 100000 hasta que se encuentren los 20 numeros que cumplan
+    for (int i = 100000; contador < 20; i++) {
+        // guardamos la raiz cuadrada en int y double para verificar si es entera
         double raiz = sqrt(i);
         int raizInt = sqrt(i);
-        // si la raiz del numero es entera, es primo y termina en 3, se imprime el numero, su division entre 14 y la raiz cuadrada
+        // si la raiz del numero es entera, es primo y termina en 7, se imprime el numero y su raiz cuadrada
         if (!(raiz - raizInt != 0) && esPrimo(raizInt) && raizInt % 10 == 7 ) {
             cout << i << " - " << raizInt << endl;
             contador++;
